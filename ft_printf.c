6a4694464d8bbd997cac6ff42e831d47a23f23d2
Francisco jesus Gata valdes <fgata-va@c3r11s6.42madrid.com>
2020-01-20 22:04:15 +0100

    Fist steps

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,50 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 13:10:38 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/01/20 15:48:10 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/01/20 21:58:53 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *, ...)
+int			manageformat(d_list *data, char *format, va_list ap)
 {
+	int i;
+
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] != '%')
+			data->printed += write(1, &format[i], 1);
+		else if (format[i] == '%')
+		{
+			while (ft_strchr(ALLSIMBOLS, format[i]))
+			{
+				i++;
+				if (ft_strchr(CONVERSIONS, format[i]))
+				{
+					//manage the conversions here
+					break ;
+				}
+				else
+					;//manage the modifiers here
+			}
+			continue;
+		}
+		i++;
+	}
+	return (data->printed);
+}
+
+int			ft_printf(const char *format, ...)
+{
+	d_list	*data;
+	va_list ap;
+
+	va_start(ap, format);
+	data = malloc(sizeof(d_list));
+	data->str = format;
+	data->printed = 0;
+	manageformat(data, format, ap);
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,28 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 13:10:34 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/01/20 13:16:13 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/01/20 21:40:56 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# define ALLSIMBOLS "cspdiuxX%-.*0123456789"
+# define CONVERSIONS "cspdiuxX%"
 # include <stdarg.h>
 # include "Libft/libft.h"
 
-int ft_printf(const char *, ...);
+typedef struct printf_s
+{
+	char	*str;
+	int		printed;
+
+	int		minus;
+	int		zero;
+	int		precision;
+	int		width;
+} 			d_list;
+
+int			ft_printf(const char *, ...);
 
 #endif
