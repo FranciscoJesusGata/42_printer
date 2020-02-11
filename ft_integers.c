cfa3d298cd0c81c30a43ab6a948fd6f0d4fc42a6
FranciscoJesusGata <fjgatv8998@gmail.com>
2020-02-11 13:33:29 +0100

    74 of 80 integers tests passed

--- ft_integers.c
+++ ft_integers.c
@@ -6,11 +6,25 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 11:58:10 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/02/08 16:34:33 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/02/11 13:32:36 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int			ft_totaldgts(t_flags *data, int dgts)
+{
+	int		diff;
+
+	diff = 0;
+	if (data->precision == 1 && data->precision_l > dgts)
+		diff = data->precision_l - dgts;
+	else if (data->zero > dgts && data->precision == 0)
+		diff = data->zero - dgts;
+	if(data->negative == 1 && (diff > 0 || data -> minus == 1))
+		diff += 1;
+	return (dgts + diff);
+}
 
 void		ft_putnbr_modified(int n, t_flags *data)
 {
@@ -57,20 +71,25 @@ void		ft_prnt_int(va_list ap, t_flags *data)
 	if(prnt < 0)
 	{
 		data->negative = 1;
-		if(data->zero > 0 || data->precision_l > 0)
-		{
-			data->printed += write(1, "-", 1);
-			dgts -= 1;
-		}
 		prnt *= -1;
 	}
-	if (data->zero > 0 && data->minus != 1)
+	if (data->width > 0 && data->minus != 1)
+	{
+		ft_width(data->width, ft_totaldgts(data, dgts), data);
+	}
+	if(data->zero > 0 && data->precision == 1)
+		ft_width(data->zero, ft_totaldgts(data, dgts), data);
+	if((data->zero > 0 || data->precision_l > 0) && data->negative == 1)
+	{
+		data->printed += write(1, "-", 1);
+		if(data->precision_l > 0)
+			dgts -= 1;
+	}
+	if (data->zero > 0 && data->minus != 1 && data->precision == 0)
 		data->printed += ft_zero(dgts, data->zero);
 	if(data->precision == 1 && data->precision_l > dgts)
 		data->printed += ft_zero(dgts, data->precision_l);
-	if (data->width > 0 && data->minus != 1)
-		ft_width(data->width, dgts, data);
 	ft_putnbr_modified(prnt, data);
 	if (data->width > 0 && data->minus == 1)
-		ft_width(data->width, dgts, data);
+		ft_width(data->width, ft_totaldgts(data, dgts), data);
 }
