33dce430010cf04365c69885bddea32d77740ab0
Francisco Jesús Gata <fgata-va@student.42madrid.com>
2021-02-17 00:10:59 +0100

    norm passed

--- Libft/ft_atol.c
+++ Libft/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 18:23:02 by fgata-va          #+#    #+#             */
-/*   Updated: 2021/02/16 23:18:28 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/02/17 00:08:43 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static long		ft_save_long(int i, const char *str, int negative)
 	while (ft_isdigit(str[i]))
 	{
 		nbr = (nbr * 10) + (str[i] - '0');
-		if ((nbr > 9223372036854775807 || nbr < -9223372036854775807) 
+		if ((nbr > 9223372036854775807 || nbr < -9223372036854775807)
 			&& negative > 0)
 			return (9223372036854775807);
 		i++;
