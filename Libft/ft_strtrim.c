5bdb6405934fc82166ad0255ae7088fe74aea47c
Francisco Jesús Gata <fgata-va@student.42madrid.com>
2021-02-16 13:23:30 +0100

    ft_strtrim fixed

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 16:52:37 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/11/26 16:53:17 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/02/16 13:21:52 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,12 @@
 static unsigned int	ft_getstart(char const *s1, char const *set)
 {
 	unsigned int	i;
-	unsigned int	j;
-	unsigned int	encountered;
 
 	i = 0;
-	j = 0;
-	encountered = 0;
 	while (s1[i] != '\0')
 	{
-		while (set[j] != '\0')
-		{
-			if (s1[i] == set[j])
-				encountered = 1;
-			j++;
-		}
-		if (encountered == 0)
+		if (!(ft_strchr(set, s1[i])))
 			return (i);
-		j = 0;
-		encountered = 0;
 		i++;
 	}
 	return (i);
@@ -41,24 +29,12 @@ static unsigned int	ft_getstart(char const *s1, char const *set)
 static size_t		ft_getlen(char const *s1, char const *set, unsigned int s)
 {
 	unsigned int	i;
-	unsigned int	j;
-	unsigned int	encountered;
 
 	i = ft_strlen(s1) - 1;
-	j = 0;
-	encountered = 0;
 	while (i > s)
 	{
-		while (set[j] != '\0')
-		{
-			if (s1[i] == set[j])
-				encountered = 1;
-			j++;
-		}
-		if (encountered == 0)
-			return (i - s + 1);
-		j = 0;
-		encountered = 0;
+		if (!(ft_strchr(set, s1[i])))
+			return (i);
 		i--;
 	}
 	return (0);
@@ -74,9 +50,10 @@ char				*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(""));
 	start = ft_getstart(s1, set);
 	len = ft_getlen(s1, set, start);
-	str = ft_substr(s1, start, len);
+	str = ft_substr(s1, start, len - start + 1);
 	if (str == NULL)
 		return (NULL);
 	str[ft_strlen(str)] = '\0';
 	return (str);
 }
+
